9201e76b11e2c3b7697ca90c038fb4f40d6a51de
deiu <andrei@fcns.eu>
2016-11-30 17:09:46 -0500

    adds the insecure env variable

--- test-run.sh
+++ test-run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export SOLIDPROXY_VERBOSE="1"
+export SOLIDPROXY_INSECURE=""
 export SOLIDPROXY_PORT="3129"
 export SOLIDPROXY_AGENT="http://example.org:3129/webid#me"
 export SOLIDPROXY_USER="http://user.com/profile#me"
