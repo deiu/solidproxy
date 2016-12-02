3beeaabf324c4af07c5f9fa763e81f95301b661e
deiu <andrei@fcns.eu>
2016-12-02 16:08:20 -0500

    fixes the path to the binary

--- test-run.sh
+++ test-run.sh
@@ -4,11 +4,11 @@ export SOLIDPROXY_VERBOSE="1"
 export SOLIDPROXY_INSECURE="1"
 export SOLIDPROXY_PROXYPORT="3129"
 export SOLIDPROXY_AGENTPORT="3200"
-export SOLIDPROXY_AGENT="https://localhost:3200/webid#me"
+export SOLIDPROXY_AGENT="http://localhost:3200/webid#me"
 export SOLIDPROXY_USER="https://alice.com/profile#me"
 
 export SOLIDPROXY_TLSKEY="test_key.pem"
 export SOLIDPROXY_TLSCERT="test_cert.pem"
-export SOLIDPROXY_ENABLETLS="1"
+export SOLIDPROXY_ENABLETLS="0"
 
-go run bin/*.go
+go run proxy-server/*.go
